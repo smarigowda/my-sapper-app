7c62210f1ad1def1ce6de24b3ab891b63c331993
Santosh Marigowda <santosharakere@gmail.com>
2019-09-06 21:07:02 +0100

    begin building layout for cards

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,16 @@
 <style>
-  h1 {
+  h1, h2 {
     text-transform: uppercase;
     text-align: center;
     font-size: 3rem;
+  }
+  .main-content {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
   }
 </style>
 
@@ -21,104 +29,100 @@
   <link rel="stylesheet" href="css/styles.css" />
 </svelte:head>
 
-<h1>Great success!</h1>
-
 <head />
-<body>
-  <div class="container">
-    <h2>Latest Entries</h2>
-    <main class="main-content">
-      <article class="entry">
-        <img src="img/01.jpg" alt="entry image" />
-        <div class="content">
-          <h3>Tips for Saving Money in your Next Travel</h3>
-          <p>
-            Published on:
-            <span>July 19th, 2019</span>
-          </p>
-          <p>
-            By:
-            <span>The Travel Blog</span>
-          </p>
-          <a href="#" class="button">Read More</a>
-        </div>
-      </article>
-      <article class="entry">
-        <img src="img/02.jpg" alt="entry image" />
-        <div class="content">
-          <h3>The Complete Guide for Traveling</h3>
-          <p>
-            Published on:
-            <span>July 19th, 2019</span>
-          </p>
-          <p>
-            By:
-            <span>The Travel Blog</span>
-          </p>
-          <a href="#" class="button">Read More</a>
-        </div>
-      </article>
-      <article class="entry">
-        <img src="img/03.jpg" alt="entry image" />
-        <div class="content">
-          <h3>Ultimate Guide to Take the best Pictures</h3>
-          <p>
-            Published on:
-            <span>July 19th, 2019</span>
-          </p>
-          <p>
-            By:
-            <span>The Travel Blog</span>
-          </p>
-          <a href="#" class="button">Read More</a>
-        </div>
-      </article>
-      <article class="entry">
-        <img src="img/04.jpg" alt="entry image" />
-        <div class="content">
-          <h3>Checklist for your next Travel</h3>
-          <p>
-            Published on:
-            <span>July 19th, 2019</span>
-          </p>
-          <p>
-            By:
-            <span>The Travel Blog</span>
-          </p>
-          <a href="#" class="button">Read More</a>
-        </div>
-      </article>
-      <article class="entry">
-        <img src="img/05.jpg" alt="entry image" />
-        <div class="content">
-          <h3>Best Places to visit next Autumn</h3>
-          <p>
-            Published on:
-            <span>July 19th, 2019</span>
-          </p>
-          <p>
-            By:
-            <span>The Travel Blog</span>
-          </p>
-          <a href="#" class="button">Read More</a>
-        </div>
-      </article>
-      <article class="entry">
-        <img src="img/06.jpg" alt="entry image" />
-        <div class="content">
-          <h3>Best Places to Visit with Mountains</h3>
-          <p>
-            Published on:
-            <span>July 19th, 2019</span>
-          </p>
-          <p>
-            By:
-            <span>The Travel Blog</span>
-          </p>
-          <a href="#" class="button">Read More</a>
-        </div>
-      </article>
-    </main>
-  </div>
-  <!--.content-->
-</body>
+<div class="container">
+  <h2>Latest Entries</h2>
+  <main class="main-content">
+    <article class="entry">
+      <img src="img/01.jpg" alt="entry image" />
+      <div class="content">
+        <h3>Tips for Saving Money in your Next Travel</h3>
+        <p>
+          Published on:
+          <span>July 19th, 2019</span>
+        </p>
+        <p>
+          By:
+          <span>The Travel Blog</span>
+        </p>
+        <a href="#" class="button">Read More</a>
+      </div>
+    </article>
+    <article class="entry">
+      <img src="img/02.jpg" alt="entry image" />
+      <div class="content">
+        <h3>The Complete Guide for Traveling</h3>
+        <p>
+          Published on:
+          <span>July 19th, 2019</span>
+        </p>
+        <p>
+          By:
+          <span>The Travel Blog</span>
+        </p>
+        <a href="#" class="button">Read More</a>
+      </div>
+    </article>
+    <article class="entry">
+      <img src="img/03.jpg" alt="entry image" />
+      <div class="content">
+        <h3>Ultimate Guide to Take the best Pictures</h3>
+        <p>
+          Published on:
+          <span>July 19th, 2019</span>
+        </p>
+        <p>
+          By:
+          <span>The Travel Blog</span>
+        </p>
+        <a href="#" class="button">Read More</a>
+      </div>
+    </article>
+    <article class="entry">
+      <img src="img/04.jpg" alt="entry image" />
+      <div class="content">
+        <h3>Checklist for your next Travel</h3>
+        <p>
+          Published on:
+          <span>July 19th, 2019</span>
+        </p>
+        <p>
+          By:
+          <span>The Travel Blog</span>
+        </p>
+        <a href="#" class="button">Read More</a>
+      </div>
+    </article>
+    <article class="entry">
+      <img src="img/05.jpg" alt="entry image" />
+      <div class="content">
+        <h3>Best Places to visit next Autumn</h3>
+        <p>
+          Published on:
+          <span>July 19th, 2019</span>
+        </p>
+        <p>
+          By:
+          <span>The Travel Blog</span>
+        </p>
+        <a href="#" class="button">Read More</a>
+      </div>
+    </article>
+    <article class="entry">
+      <img src="img/06.jpg" alt="entry image" />
+      <div class="content">
+        <h3>Best Places to Visit with Mountains</h3>
+        <p>
+          Published on:
+          <span>July 19th, 2019</span>
+        </p>
+        <p>
+          By:
+          <span>The Travel Blog</span>
+        </p>
+        <a href="#" class="button">Read More</a>
+      </div>
+    </article>
+  </main>
+</div>
+<!--.content-->
